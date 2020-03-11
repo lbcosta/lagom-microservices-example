b28de22d61314f338d60517c86296e7b64c27cc1
Leonardo Costa <lbcosta.dev@gmail.com>
2020-03-11 18:46:03 -0300

    @Front-End/Proposta 2 - Nova função no menu lat

--- webapp/src/components/SideMenu/index.tsx
+++ webapp/src/components/SideMenu/index.tsx
@@ -1,33 +1,57 @@
-import React, { FunctionComponent, useContext } from 'react';
-import cx from 'classnames';
-import mainCtx from '../../contexts/mainCtx';
-import InsightLogo from '../../assets/logo-insight.png';
+import React, {
+  FunctionComponent,
+  useContext,
+  useEffect,
+  useState
+} from "react";
+import cx from "classnames";
+import mainCtx from "../../contexts/mainCtx";
+import InsightLogo from "../../assets/logo-insight.png";
+import BusesService from "../../services/buses";
 import "./styles.scss";
 
 const SideMenu: FunctionComponent = props => {
-    const { menuOpen, setMenuOpen } = useContext(mainCtx);
+  const { menuOpen, setMenuOpen } = useContext(mainCtx);
+  const [apiVersion, setApiVersion] = useState("");
 
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-    return (
-        <div className={cx("side-menu-container", {
+  useEffect(() => {
+    async function fetchApiVersion() {
+      const response = await BusesService.getApiVersion();
+      setApiVersion(response.data["sptransVersion"]);
+    }
+
+    fetchApiVersion();
+  }, []);
+
+  return (
+    <div
+      className={cx("side-menu-container", {
+        open: menuOpen
+      })}
+    >
+      <div onClick={toggleMenu} className="menu-icon">
+        <span className="menu-icon-line" />
+        <span className="menu-icon-line" />
+        <span className="menu-icon-line" />
+      </div>
+      <div className="side-menu-body"></div>
+      <div className="side-menu-footer">
+        <p
+          className={cx("api-version", {
             open: menuOpen
-        })}>
-            <div onClick={toggleMenu} className="menu-icon">
-                <span className="menu-icon-line" />
-                <span className="menu-icon-line" />
-                <span className="menu-icon-line" />
-            </div>
-            <div className="side-menu-body">
-                
-            </div>
-            <div className="side-menu-footer">
-                <img src={InsightLogo} alt='Logotipo do Insight Data Science Lab' />
-            </div>
-        </div>
-    );
+          })}
+        >
+          SPTrans - API Olho Vivo - Versão: {apiVersion}
+        </p>
+
+        <img src={InsightLogo} alt="Logotipo do Insight Data Science Lab" />
+      </div>
+    </div>
+  );
 };
 
 export default SideMenu;
